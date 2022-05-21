c917e767f5689d050caf2fde1a4a19c614794803
AVKostin <90350582+AVKostin@users.noreply.github.com>
2022-05-21 21:22:56 +0000

    Deploying to gh-pages from @ AVKostin/my-resume@d2c241b21708b4967b9838b1a556deacb3565027 🚀

--- src.d3f6e08a.js
+++ src.d3f6e08a.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./scss/main.scss");
 },{"./scss/main.scss":"fx60"}]},{},["Focm"], null)
-//# sourceMappingURL=/my-resume/src.c9eed6c3.js.map
+//# sourceMappingURL=/my-resume/src.d3f6e08a.js.map
